02a5b18b56838be0aad3732edce28d3f25b41102
iliik <koselevicila@gmail.com>
2022-07-30 15:59:24 +0300

    it-kamasutra

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,52 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
+    return (
+        <div>
+            Hello
+            <Star/>
+            <Accordion/>
+        </div>
+    );
+}
+
+function Star() {
+    return (
+        <div>Star</div>
+    );
+}
+
+function Accordion() {
+    console.log("Accordion rendering")
+    return (
+
     <div>
-      Hello
-        <Star/>
-      <Accordion/>
       <AccordionTitle/>
       <AccardionBody/>
     </div>
-  );
+);
 }
 
-function Star () {
-  return (
-      <div>Star</div>
-  );
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return (
+        <div>
+            <h3>Меню</h3>
+        </div>
+    );
 }
 
-function Accordion () {
-  console.log("Accordion rendering")
-  return (
-      <div></div>
-  );
-}
-
-function AccordionTitle () {
-  console.log("AccordionTitle rendering")
-  return (
-    <div>
-      <h3>Меню</h3>
-    </div>
-  );
-}
-
-function AccardionBody () {
-  console.log("AccardionBody rendering")
-  return (
-      <div>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
-      </div>
-  );
+function AccardionBody() {
+    console.log("AccardionBody rendering")
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
