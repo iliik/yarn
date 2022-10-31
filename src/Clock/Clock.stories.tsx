6f69a292b8adaa0a5b19b95a38fd525a749bbbea
Ilya <koselevicila@gmail.com>
2022-10-31 19:16:42 +0300

    useEffect-Clock

--- src/Clock/Clock.stories.tsx
+++ src/Clock/Clock.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import {Clock} from "./Clock";
 
 export default {
--- src/Clock/Clock.tsx
+++ src/Clock/Clock.tsx
@@ -1,16 +1,23 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
-type PropsType={
+type PropsType = {}
 
-}
 
-export const Clock :React.FC<PropsType> = () =>{
+const get2diqitsString = (number: number) => number < 10 ? "0" + number : number
+export const Clock: React.FC<PropsType> = () => {
     const [date, setDate] = useState(new Date())
-    return<div>
-        <span>{date.getHours()}</span>
+
+    useEffect(() => {
+        setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+    }, [])
+
+    return <div>
+        <span>{get2diqitsString(date.getHours())}</span>
         :
-        <span>{date.getMinutes()}</span>
+        <span>{get2diqitsString(date.getMinutes())}</span>
         :
-        <span>{date.getSeconds()}</span>
+        <span>{get2diqitsString(date.getSeconds())}</span>
     </div>
 }
